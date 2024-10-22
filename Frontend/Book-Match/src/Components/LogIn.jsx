4bd5a33e279765fc2c5f0e4473caae097bd5cb24
Katie Park <147112323+KJP952@users.noreply.github.com>
2024-10-21 23:36:54 -0400

    Login

--- Frontend/Book-Match/src/Components/LogIn.jsx
+++ Frontend/Book-Match/src/Components/LogIn.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import "./Login.css";
 
 const LogIn = () => {
     return (
-        <div>
-            <h1>Log In Page</h1>
-            <p>This is the page content</p>
-        </div>
-    );
+        // <div>
+        //     <h1>Log In Page</h1>
+        //     <p>This is the page content</p>
+        // </div>
+
+        <>
+            <div className="LoginBox">
+                <h3>Welcome Back</h3>
+                <h4>Please enter your details</h4>
+
+                <form>
+                    <label for="eadress">Email Adress</label>
+                    <input type="text" id="eadress" name="eadress" />
+                    <label for="pword">Password</label>
+                    <input type="text" id="pword" name="pword" />
+                </form>
+            </div>
+            
+            <div className="logo">
+            </div></>
+    );  
 };
 
 export default LogIn;
